a8bb05300775c1e8b3084845f0573be53601825c
David Crawford <crawford@mail.gvsu.edu>
2017-10-30 01:32:52 -0400

    OAuth now functions properly and user can log in and receive a unique token.

--- api.js
+++ api.js
@@ -1,40 +1,50 @@
-$(window).load(function() {
-  $.ajax({
-    method: "GET",
-    origin: 'https://davealdon.github.io/AJAX-Demo/',
-    url: "https://accounts.spotify.com/authorize",
-    data: {
-      client_id: 'aadca442888c4fe6b93573a75d8ecb45',
-      response_type: 'token',
-      redirect_uri: 'https://davealdon.github.io/AJAX-Demo/'
-    },
-    success: function(result) {
-      console.log(result);
-      // handle result...
-    },
-  });
-  var searchArtists = function (query) {
-      $.ajax({
-          url: 'https://api.spotify.com/v1/search',
-          data: {
-              q: query,
-              client_id: 'aadca442888c4fe6b93573a75d8ecb45',
-              type: 'Artist',
-              access_token: 'BQDMbtf60uMji4u6UhM-IScPQhv7FiZ1xdW7ctffanir2QC4poFDSPCMPGF3HGwenG01tCjzU48nS3RsXUdDkrX-e2cRTjSaKb0E0YUPdzhGGloRvCfqvPY0gy2sl1uAhr9sqtr9Wrwmy4ri',
-              token_type: 'bearer',
-          },
-          success: function (response) {
-            $response = JSON.parse(JSON.stringify(response));
-            $imageUrl = $response.artists.items[0].images[0].url;
-            console.log($imageUrl);
-            $('#results').html($('<img>',{id:'artistPicture',src:$imageUrl}))
-          }
-      });
-      console.log("asdsda");
-  };
+$.urlParam = function(name){
+  results = new RegExp('(' + name + '=)([^&#]*)').exec(window.location.href);
+  if (results==null){
+     return null;
+  }
+  else {
+    console.log(results);
+    return decodeURI(results[2]) || 0;
+  }
+}
 
-  document.getElementById('search-form').addEventListener('submit', function (e) {
-        e.preventDefault();
-        searchArtists(document.getElementById('query').value);
-  }, false);
+var searchArtists = function (query) {
+  $.ajax({
+      url: 'https://api.spotify.com/v1/search',
+      data: {
+          q: query,
+          client_id: 'aadca442888c4fe6b93573a75d8ecb45',
+          type: 'Artist',
+          access_token: $.urlParam('access_token'),
+          token_type: 'bearer',
+      },
+      success: function (response) {
+        imageUrl = response.artists.items[0].images[0].url;
+        console.log(imageUrl);
+        $('#results').html($('<img>',{id:'artistPicture',src:imageUrl}))
+      }
+  });
+};
+
+$(document).ready(function() {
+  $("#login").click(function(){
+    $.ajax({
+      method: "GET",
+      url: "https://accounts.spotify.com/authorize",
+      data: {
+        client_id: 'aadca442888c4fe6b93573a75d8ecb45',
+        response_type: 'token',
+        redirect_uri: 'https://davealdon.github.io/AJAX-Demo/'
+      },
+      complete : function(){
+        $(location).attr('href', this.url)
+      }
+    });
+  });
+
+  $("#search").click(function(){
+    searchArtists($("#query").val());
+    $("#query").val('');
+  });
 });
